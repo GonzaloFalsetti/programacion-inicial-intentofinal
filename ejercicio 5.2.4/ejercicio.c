097e05ae8b45fbdbd3086db5c8385bb839b11499
GonzaloFalsetti <gonza3789r@gmail.com>
2024-10-22 20:03:23 -0300

    22-10

--- ejercicio 5.2.4/ejercicio.c
+++ ejercicio 5.2.4/ejercicio.c
@@ -1,22 +1,62 @@
+/*Un negocio de venta de granos desea controlar las ventas realizadas. De cada una ingresa el importe total
+y un código que indica la forma de pago. El código puede ser:
+• C: cheque, 20% de recargo.
+• E: efectivo, 10% de descuento.
+• T: con tarjeta, 12% de recargo.
+Se debe ingresar una F para finalizar el día de venta y arrojar los siguientes totales.
+Efectivo en Caja: $ xxxx.xx
+Ventas con Tarjeta de Crédito: $ xxxx.xx
+Ventas con cheque: $ xxxx.xx
+Total de Venta: $ xxxx.xx
+Importe del IVA: $ xxxx.xx
+Nota: El IVA corresponde al 21% del total de ventas..
+*/
+
 #include <stdio.h>
 
-int main() {
-    int N, i = 1;
+int main ()
+{
+    int c=120  , t=112 , valordecompra=0 , efectivoencaja=0 , totaltarjetadecredito=0 ,credito=0,cheque=0, totalefectivo=0, totalcheques=0 , totaldelaventa=0 , importeiva=0;
+    char tipodepago;
     
-    // Pedir al usuario el número de filas
-    printf("Ingrese el número de filas: ");
-    scanf("%d", &N);
+    printf ("\n ingrese el tipo de pago de la compra e,t,c: ");
+        fflush (stdin);
+        scanf ("%c",&tipodepago);
+    while (tipodepago != 'f')
+    {
 
-    // Ciclo para imprimir el triángulo
-    
-        // Ciclo interno para imprimir los asteriscos
-        for (int j = 1; j <= i; j++) {
-            printf("*");
-        }
-        // Imprimir un salto de línea después de cada fila
-        printf("\n");
         
+        printf ("\n ingrese el valor de la compra: ");
+        scanf ("%d",&valordecompra);
 
+        if (tipodepago == 'e')
+        {
+            efectivoencaja = valordecompra - (valordecompra * 0.1);
+            totalefectivo += efectivoencaja;
+        }
+        if (tipodepago == 't')
+        {
+            credito = (valordecompra *t)/100;
+            totaltarjetadecredito += credito ;
+        }
+        if (tipodepago == 'c')
+        {
+            cheque = (valordecompra * c)/100;
+            totalcheques += cheque;
+        }
+
+        totaldelaventa = (totalcheques + totaltarjetadecredito + totalefectivo);
+        importeiva = (totaldelaventa * 0.21);
+
+        printf ("\n ingrese el tipo de pago de la compra e,t,c: ");
+        fflush (stdin);
+        scanf ("%c",&tipodepago);
+
+    }
+    printf ("\nEfectivo en Caja: %d \nVentas con Tarjeta de Crédito: %d \nVentas con cheque: %d \nTotal de Venta: %d \nImporte del IVA: %d",totalefectivo,totaltarjetadecredito,totalcheques,totaldelaventa,importeiva);
     
+
+
+
     return 0;
 }
---
+++ ejercicio 5.2.5/ejercicio.c
@@ -0,0 +1,32 @@
+/*De cada uno de los alumnos de un curso se ingresa su número de DNI y las notas de los dos exámenes 
+parciales confeccionar, un programa que muestre por cada alumno su condición final con el siguiente 
+mensaje: 
+“El alumno con DNI: xxxxxxx, obtuvo las notas: xx y xx [condición final]”
+La condición final se establece según las siguientes reglas:
+• PROMOCIONA: ambas notas > = 7
+• RINDE EXAMEN FINAL: ambas notas > = 4
+• REPROBO LA MATERIA: alguna nota no cumple lo anterior
+El programa finaliza cuando se ingresa un valor negativo como número de DNI. Al finalizar mostrar la nota 
+promedio de alumnos promocionados..*/
+
+
+#include <stdio.h>
+
+int main ()
+{
+    int dni=0,nota1=0,nota2=0;
+    float notapromedio=0;
+
+
+    do
+    {
+        
+    } while (dni >= 0);
+    
+
+
+
+
+
+    return 0;
+}
